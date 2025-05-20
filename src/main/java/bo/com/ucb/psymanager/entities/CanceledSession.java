2e49d463ac125ba645ff0db94d63b6ec31e5a3b0
BenitoAR10 <amir.rojas@ucb.edu.bo>
2025-05-20 19:29:30 -0400

    Trabajando en el desarrollo de la entidad cancel session

---
+++ src/main/java/bo/com/ucb/psymanager/entities/CanceledSession.java
@@ -0,0 +1,34 @@
+package bo.com.ucb.psymanager.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ps_canceled_session")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CanceledSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "canceled_session_id")
+    private Long canceledSessionId;
+
+    @Column(name = "cancellation_date", nullable = false)
+    private LocalDateTime cancellationDate;
+
+    @Column(name = "cancellation_reason", columnDefinition = "TEXT", nullable = false)
+    private String cancellationReason;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "canceled_by_user_id", nullable = false)
+    private User canceledBy;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_session_id", nullable = false)
+    private ScheduleSession scheduledSession;
+}
